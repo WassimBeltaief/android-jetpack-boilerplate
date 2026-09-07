c4fd46f3f04c9e5ae4260b3b05f35aa59f448cf4
Wassim Beltaief <wassim.beltaief@adevinta.com>
2026-09-07 18:04:38 +0200

    feat: add core:designsystem Material3 theme

---
+++ core/designsystem/build.gradle.kts
@@ -0,0 +1,9 @@
+plugins {
+    id("boilerplate.android.library.compose")
+}
+
+dependencies {
+    api(libs.androidx.compose.material3)
+    api(libs.androidx.compose.foundation)
+    api(libs.androidx.compose.runtime)
+}
