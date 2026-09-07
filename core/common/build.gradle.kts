b236140dd081516dd88317e92930728992d60187
Wassim Beltaief <wassim.beltaief@adevinta.com>
2026-09-07 18:04:18 +0200

    feat: add core:common

---
+++ core/common/build.gradle.kts
@@ -0,0 +1,8 @@
+plugins {
+    id("boilerplate.android.library")
+    id("boilerplate.android.hilt")
+}
+
+dependencies {
+    api(libs.kotlinx.coroutines.android)
+}
---
+++ core/common/src/main/kotlin/com/example/boilerplate/core/common/network/NetworkResult.kt
@@ -0,0 +1,17 @@
+package com.example.boilerplate.core.common.network
+
+sealed interface NetworkResult<out T> {
+    data class Success<T>(val data: T) : NetworkResult<T>
+    data class Error(val code: Int = -1, val message: String? = null) : NetworkResult<Nothing>
+    data object Loading : NetworkResult<Nothing>
+}
+
+inline fun <T> NetworkResult<T>.onSuccess(action: (T) -> Unit): NetworkResult<T> {
+    if (this is NetworkResult.Success) action(data)
+    return this
+}
+
+inline fun <T> NetworkResult<T>.onError(action: (code: Int, message: String?) -> Unit): NetworkResult<T> {
+    if (this is NetworkResult.Error) action(code, message)
+    return this
+}
