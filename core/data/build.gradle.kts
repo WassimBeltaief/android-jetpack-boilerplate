b6fe12f91c4a2d737f192de61d87604285e28d38
Wassim Beltaief <wassim.beltaief@adevinta.com>
2026-09-07 19:47:42 +0200

    fix: add missing test dependencies to core:data module

--- core/data/build.gradle.kts
+++ core/data/build.gradle.kts
@@ -10,4 +10,6 @@ dependencies {
     implementation(project(":core:database"))
     implementation(project(":core:domain"))
     implementation(libs.kotlinx.coroutines.android)
+
+    testImplementation(project(":core:testing"))
 }
